da3e1b61a6531fd1103a334c03f367fddd2f6c46
RomiChen <429650229@qq.com>
2025-06-05 18:11:45 +0800

    修正控制台输出格式，调整DW_FLAGS最后一个标志的打印信息. BugBot run

--- src/bug.ts
+++ src/bug.ts
@@ -12,4 +12,4 @@ export { getLastFlag };
 export const lastFlag = DW_FLAGS[DW_FLAGS.length];
 
 // 打印最后一个标志（用于调试）
-console.log('DW_FLAGS的最后一个标志是:', lastFlag); 
+console.log('DW_FLAGS的最后一个标志是: ', lastFlag); 
