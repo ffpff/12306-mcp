0c4f55f6709cfa7b427ec1cb40ca7471f8adef3e
RomiChen <429650229@qq.com>
2025-06-05 18:06:31 +0800

    验证bugbot功能。BugBot run

---
+++ src/bug.ts
@@ -0,0 +1,15 @@
+import { DW_FLAGS } from './constants.js';
+
+// 获取DW_FLAGS的最后一个标志
+const getLastFlag = (): string => {
+  return DW_FLAGS[DW_FLAGS.length];
+};
+
+// 导出函数
+export { getLastFlag };
+
+// 直接获取最后一个标志
+export const lastFlag = DW_FLAGS[DW_FLAGS.length];
+
+// 打印最后一个标志（用于调试）
+console.log('DW_FLAGS的最后一个标志是:', lastFlag); 
